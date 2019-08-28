d2304a13549eaa8fa239ae688b640ece88584039
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-28 18:16:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,12 @@
 class School
-  attr_reader :roster
+  attr_reader :roster, :name
   def initialize(name)
-    @roster = {}
+    @name = name
+    @roster = {9=>["Steve", "John"], 8=>["Lilly"]}
+  end
+  def add_student(student_name, grade)
+    @roster[grade] = []
+    @roster[grade] << student_name
+  
   end
 end
